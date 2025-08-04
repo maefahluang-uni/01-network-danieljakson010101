e0f6304f52bbe2cccb89783eb42770cf00e17330
danieljakson010101 <6831503903@lamduan.mfu.ac.th>
2025-08-04 16:05:36 +0700

    Update MockWebServer.java

--- src/main/java/th/mfu/MockWebServer.java
+++ src/main/java/th/mfu/MockWebServer.java
@@ -15,22 +15,33 @@ public class MockWebServer implements Runnable {
     public void run() {
 
         // TODO Create a server socket bound to specified port
-
+        try (ServerSocket serverSocket = new ServerSocket(port)){
+            
         System.out.println("Mock Web Server running on port " + port + "...");
 
         while (true) {
             // TODO Accept incoming client connections
-
+            Socket clientSocket = serverSocket.accept();
+            
             // TODO Create input and output streams for the client socket
-
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            
             // TODO: Read the request from the client using BufferedReader
-
+            String requestLine = in.readLine();
+            System.out.println("Received request: " + requestLine);
+            
             // TODO: send a response to the client
             String response = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n"
                     + "<html><body>Hello, Web! on Port " + port + "</body></html>";
-
+            out.println(response);
+            
             // TODO: Close the client socket
-
+            clientSocket.close();
+            }
+            } catch (IOException e) {
+            System.err.println("Error in MockWebServer: " + e.getMessage());
+            e.printStackTrace();
         }
 
     }
